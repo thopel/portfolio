4880c62fa28ef6ea9bc22c96c91beb9b91447fce
PELFRENE <87179999+ThoPel@users.noreply.github.com>
2022-02-09 17:33:56 +0100

    Style: Refonte page Projet

--- pages/_name.vue
+++ pages/_name.vue
@@ -1,64 +1,45 @@
 <template>
   <main v-if="Loaded && technoLoaded">
-    <h1>{{ projet.name }}</h1>
-    <div class="row">
-      <div class="column">
-        <div class="nom">
-          <!-- <p>{{projet.annee.substring(0, 2)}}<br/>{{projet.annee.substring(2, 4)}}</p> -->
-        </div>
-        <h2>{{ projet.annee }} - {{ projet.type }}</h2>
-        <p class="descr">
-          {{ projet.description }}
+    <div class="top">
+      <h1>{{ projet.name }}</h1>
+      <h2>{{ projet.annee }}</h2>
+    </div>
+    <h3>{{ projet.type }}</h3>
+    <div v-if="projet.collab.length > 0" class="collab">
+      <p>Collaborateur.trice(s):</p>
+      <a
+        v-for="(pers, index) in projet.collab"
+        :key="index"
+        target="_blank"
+        :href="'https://www.linkedin.com/in/' + pers.linkedin"
+        >{{ pers.name }}</a
+      >
+    </div>
+    <div class="tech">
+      <div class="logo-app" v-for="(app, index) in projet.techno" :key="index">
+        <img
+          :src="techno.find((elem) => elem.id == projet.techno[index]).url"
+        />
+        <p>
+          {{ techno.find((elem) => elem.id == projet.techno[index]).name }}
         </p>
-        <a v-if="projet.lien != ''" class="link" :href="projet.lien"
-          >› visiter le site</a
-        >
-        <div v-if="projet.collab.length > 0" class="collab">
-          <p>Collaborateur.trice(s):</p>
-          <a
-            v-for="(pers, index) in projet.collab"
-            :key="index"
-            target="_blank"
-            :href="'https://www.linkedin.com/in/' + pers.linkedin"
-            >{{ pers.name }}</a
-          >
-        </div>
-        <div class="tech">
-          <div
-            class="logo-app"
-            v-for="(app, index) in projet.techno"
-            :key="index"
-          >
-            <img
-              :src="techno.find((elem) => elem.id == projet.techno[index]).url"
-            />
-            <p>
-              {{ techno.find((elem) => elem.id == projet.techno[index]).name }}
-            </p>
-          </div>
-        </div>
       </div>
-      <div class="slider">
-        <div class="dots">
-          <a
-            id="fleche"
-            @mousedown="addMove"
-            @click="
-              indexSlider == projet.images.length - 1
-                ? (indexSlider = 0)
-                : indexSlider++
-            "
-            :href="'#img-' + indexSlider"
-            :key="index"
-            >→</a
-          >
-        </div>
-        <div class="slides">
-          <div>
-            <img :src="projet.images[indexSlider]" alt="slide" />
-          </div>
-        </div>
-      </div>
+    </div>
+    <p class="descr">
+      {{ projet.description }}
+    </p>
+    <a v-if="projet.lien != ''" class="link" :href="projet.lien"
+      >› se rendre sur le projet</a
+    >
+
+    <div class="wrapper">
+      <img
+        v-for="(img, index) in projet.images"
+        :key="index"
+        class="picture"
+        :style="'width:' + (img.width - 0.25) + '%'"
+        :src="img.url"
+      />
     </div>
   </main>
 </template>
@@ -147,94 +128,108 @@ main {
   position: relative;
   display: flex;
   flex-direction: column;
-  padding-top: 50px;
+  padding-top: 3.472vw;
   width: 100%;
   height: fit-content;
-
-  @include tablet {
-    height: 100%;
-  }
-
-  .row {
-    display: flex;
-    justify-content: space-between;
-
-    @include tablet {
-      flex-direction: column-reverse;
-    }
-  }
 
   h1 {
     font-size: 6rem;
     letter-spacing: 5px;
     text-transform: uppercase;
     font-family: $Readex-Medium;
+    width: 100%;
 
-    @include tablet {
-      font-size: 3rem;
+    @include mobile {
+      font-size: 2.5rem;
     }
   }
 
-  .column {
-    display: flex;
-    flex-direction: column;
-  }
-
-  .nom {
-    margin-bottom: 40px;
-
-    p {
-      font-size: 2rem;
-      margin-right: 20px;
-      font-family: $Readex-Regular;
-    }
-  }
-
-  h2 {
-    margin: 15px 0;
+  h2,
+  h3 {
     font-family: $Amperzand;
     font-weight: normal;
     text-transform: capitalize;
-    font-size: 4rem;
+    font-size: 5rem;
+
+    @include mobile {
+      font-size: 2.5rem;
+    }
   }
 
-  .link {
-    font-size: 1.5rem;
+  h3 {
+    margin-top: 2vw;
+  }
+
+  .top {
+    width: 100%;
+    @include flexbox(row, space-between, flex-end);
+
+    @include mobile {
+      @include flexbox(column, flex-start, flex-start);
+    }
+  }
+
+  .collab {
+    margin-top: 3vw;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    font-size: 2.5rem;
     font-family: $Readex-Regular;
-    text-decoration: none;
-    color: inherit;
-    margin-top: 20px;
-    margin-left: 20px;
-    width: fit-content;
+
+    @include mobile {
+      font-size: 1.5rem;
+    }
+
+    a {
+      margin-left: 10px;
+      color: var(--color1);
+    }
   }
 
   .descr {
-    max-width: 30vw;
-    font-size: 1.5rem;
+    font-size: 3rem;
     font-family: $Readex-Regular;
+    width: 85%;
+    margin-top: 3vw;
+    margin-bottom: 1vw;
 
     @include tablet {
-      max-width: 100%;
+      width: 100%;
+      text-align: justify;
     }
 
-    p {
-      @include tablet {
-        width: 100%;
-      }
+    @include mobile {
+      font-size: 2rem;
+      margin-top: 10vw;
+    }
+  }
+
+  .link {
+    font-size: 2.6rem;
+    font-family: $Readex-Regular;
+    text-decoration: none;
+    color: inherit;
+    margin-left: 2vw;
+    margin-bottom: 3vw;
+    width: fit-content;
+
+    @include mobile {
+      font-size: 2rem;
+      margin-top: 7vw;
     }
   }
 
   .tech {
-    display: grid;
-    grid-template-columns: repeat(5, 1fr);
-    grid-column-gap: 10px;
-    grid-row-gap: 10px;
+    @include flexbox(row, flex-start, center, 3vw);
     margin-top: 50px;
-    font-size: 1.5rem;
+    font-size: 2rem;
     font-family: $Readex-Regular;
+    flex-wrap: wrap;
 
     @include mobile {
-      grid-template-columns: repeat(4, 1fr);
+      font-size: 1.5rem;
+      gap: 6vw;
     }
   }
 
@@ -247,8 +242,12 @@ main {
     align-items: center;
     img {
       width: auto;
-      height: 50px;
+      height: 70px;
       margin-bottom: 5px;
+
+      @include mobile {
+        height: 40px;
+      }
     }
     p {
       width: 100%;
@@ -256,119 +255,21 @@ main {
     }
   }
 
-  .collab {
-    margin-top: 20px;
-    display: flex;
-    flex-direction: row;
+  .wrapper {
+    @include flexbox(row, flex-start, flex-start, 0.5%);
     flex-wrap: wrap;
-    width: 30vw;
-    font-size: 1.5rem;
-    font-family: $Readex-Regular;
+    margin-top: 3vw;
 
-    @include tablet {
-      width: 100%;
+    @include mobile {
+      margin-top: 10vw;
     }
 
-    a {
-      margin-left: 10px;
-      color: var(--color1);
-    }
-  }
-}
+    .picture {
+      height: auto;
+      margin-bottom: 0.5%;
 
-.slider {
-  display: flex;
-  flex-direction: column;
-  width: fit-content;
-  max-width: 62vw;
-  text-align: center;
-  overflow: hidden;
-  position: relative;
-  z-index: 1;
-
-  @include tablet {
-    max-width: 100%;
-  }
-
-  @include mobile {
-    justify-content: center;
-    flex-direction: column-reverse;
-    margin-top: 20px;
-    width: 100%;
-  }
-}
-
-.slides > div {
-  width: 100%;
-  height: auto;
-  transform-origin: center center;
-  transform: scale(1);
-  position: relative;
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-
-  @include mobile {
-    justify-content: center;
-  }
-
-  img {
-    width: auto;
-    height: 50vh;
-
-    @include tablet {
-      height: 53vw;
-    }
-  }
-}
-
-.slider .dots {
-  width: 100%;
-  display: flex;
-  justify-content: flex-end;
-  padding-right: 20px;
-  padding-top: 5px;
-
-  @include mobile {
-    padding-right: 0;
-  }
-}
-
-.slider .dots a {
-  display: inline-flex;
-  width: fit-content;
-  text-decoration: none;
-  align-items: center;
-  justify-content: center;
-  letter-spacing: 2px;
-  margin: 0 5px;
-  font-size: 7rem;
-  position: relative;
-  color: inherit;
-  font-family: $Readex-Regular;
-
-  &:hover {
-    font-family: $Readex-Medium;
-  }
-
-  &.move {
-    animation: move 1s ease-in-out;
-
-    @keyframes move {
-      0% {
-        transform: translateX(0);
-      }
-      25% {
-        transform: translateX(10px);
-      }
-      50% {
-        transform: translateX(-5px);
-      }
-      75% {
-        transform: translateX(3px);
-      }
-      100% {
-        transform: translateX(0);
+      @include mobile {
+        width: 100% !important;
       }
     }
   }
